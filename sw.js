560d46a66945cdddd089e553b2f45798c182bb34
dobladov <danieldoblado@gmail.com>
2020-01-12 17:37:53 +0000

    Deploying to gh-pages from  db5ce391491584f9dfb40edf511e3819f98e5d8d

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-importScripts('/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/Vuerama/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/05952600a6272d1d7a05.js",
+    "url": "/Vuerama/05952600a6272d1d7a05.js",
     "revision": "18d3f7b005433cea6d934d6dc6ecd747"
   },
   {
-    "url": "/_nuxt/0c136c6efc59e646ca60.js",
-    "revision": "95b69dc697ec7bc95890a7d84c75eae4"
-  },
-  {
-    "url": "/_nuxt/14677cbafcc23b819a52.js",
+    "url": "/Vuerama/14677cbafcc23b819a52.js",
     "revision": "e48b868f0621dcf89d6a4acc86722597"
   },
   {
-    "url": "/_nuxt/8b876074a32bc2763f2d.js",
-    "revision": "10a0aab799dd3a1aae0b1ea23020407b"
+    "url": "/Vuerama/27823187a1c07338e878.js",
+    "revision": "8881371143443b74f66d8599b8ef0238"
   },
   {
-    "url": "/_nuxt/d045fe7b7affb683eb9a.js",
+    "url": "/Vuerama/d045fe7b7affb683eb9a.js",
     "revision": "9845e07dc0b019b10d813b694e767738"
+  },
+  {
+    "url": "/Vuerama/e7c3ee877ab989fff8f3.js",
+    "revision": "8e75345738265148c410f78aa4d2c72e"
   }
 ], {
   "cacheId": "vuerama",
@@ -30,6 +30,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/Vuerama/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
 workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
